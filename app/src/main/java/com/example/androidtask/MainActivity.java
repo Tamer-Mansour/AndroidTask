476c30aaf8fb88e5a0c30ad9d9791e5d28f90844
Tamer-Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2021-07-06 16:54:38 +0300

    First Update

--- app/src/main/java/com/example/androidtask/MainActivity.java
+++ app/src/main/java/com/example/androidtask/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.androidtask;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
